c51dc1e8775a0cef4029e3fcd37877484d50e442
Yanu <59870900+khusanova@users.noreply.github.com>
2026-03-24 16:59:21 +0100

    Add exceptions handling to load_data function.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,5 +11,15 @@ def load_data(file_path: str):
     Returns:
         Data loaded from the JSON file.
     """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    try:
+        with open(file_path, "r", encoding="utf-8") as handle:
+            return json.load(handle)
+    except FileNotFoundError:
+        print(f"{file_path} does not exist.")
+    except PermissionError:
+        print(f"Cannot read file {file_path}. Permission denied.")
+    except UnicodeDecodeError:
+        print(f"Cannot read file {file_path}. Encoding should be UTF-8.")
+    except OSError as e:
+        print(f"Failed to load {file_path}: {e}")
+    return None
